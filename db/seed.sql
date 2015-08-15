1b4a16227e46613351580575698dc593de39b6ea
Sam Haycraft <phreakdawg@gmail.com>
2015-08-15 04:06:27 +0000

    fixes client seed table

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO users.users(username, firstname, lastname, password, email)
 VALUES ('brewuser', 'brew','user',encode(digest('meow', 'SHA1'),'hex'),'brew@brew.com');
 
-INSERT INTO users.clients(clientId,name,secret)
+INSERT INTO users.clients(id,name,secret)
 VALUES (1,'Default Client', 'secret');

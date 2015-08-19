0bd730965eb12289c09980995c028c30d6b3b565
Sam <phreakdawg@gmail.com>
2015-08-18 20:44:33 -0600

    better seeding by checking if seed already exists

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,21 @@
-INSERT INTO users.users(username, firstname, lastname, password, email)
-VALUES ('brewuser', 'brew','user',encode(digest('meow', 'SHA1'),'hex'),'brew@brew.com');
+do $$
+DECLARE row_count INTEGER DEFAULT 0;
+BEGIN
 
-INSERT INTO users.clients(clientId,name,secret)
-VALUES (1,'Default Client', 'secret');
+IF NOT EXISTS  (SELECT 1 FROM users.users WHERE username='brewuser') THEN 
+	INSERT INTO users.users(username, firstname, lastname, password, email)
+	VALUES ('brewuser', 'brew','user',encode(digest('meow', 'SHA1'),'hex'),'brew@brew.com');
+	GET DIAGNOSTICS row_count = ROW_COUNT;
+END IF;
+RAISE NOTICE 'Inserted % rows into users.users.',row_count;
+
+row_count := 0;
+IF NOT EXISTS (SELECT 1 FROM users.clients where id=1) THEN
+	INSERT INTO users.clients(id,name,secret)
+	VALUES (1,'Default Client', 'secret');
+	GET DIAGNOSTICS row_count = ROW_COUNT;
+END IF;
+RAISE NOTICE 'Inserted % rows into users.clients.',row_count;
+
+END;
+$$

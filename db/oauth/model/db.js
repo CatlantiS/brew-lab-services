6ea7ac8a70743f03df656882b4e8d141c7223b20
dustmouse <paul.palubinski@gmail.com>
2015-10-13 15:49:07 -0600

    Merged changes.

--- db/oauth/model/db.js
+++ db/oauth/model/db.js
@@ -1,6 +1,4 @@
 	var connectionString = require('../../configuration/dbConfig.js').connectionString;
-	module.exports.conn = require('../../core/pgConnect')(connectionString);
-	var connectionString = 'postgres://postgres:samcool@shaycraft.cloudapp.net/brewlabdb';
 	module.exports.conn = require('../../core/pgConnection')(connectionString);
 	module.exports.queries = require('../../helpers/queries');
 

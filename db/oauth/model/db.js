fcb2bd6320b4b1c08b233044b0ef0336065e17e5
Sam <phreakdawg@gmail.com>
2015-10-13 10:47:36 -0600

    oauth connection string now using main dbconfig.js

--- db/oauth/model/db.js
+++ db/oauth/model/db.js
@@ -1,4 +1,4 @@
-	var connectionString = 'postgres://postgres:samcool@shaycraft.cloudapp.net/brewlabdb';
+	var connectionString = require('../../configuration/dbConfig.js').connectionString;
 	module.exports.conn = require('../../core/pgConnect')(connectionString);
 	module.exports.queries = require('../../helpers/queries');
 
